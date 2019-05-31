40a48aed2ba26309882d199d523189131ef617cd
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-05-31 10:00:18 +0800

    Agregando clase Contexto y poniendo public la clase de productos

---
+++ Parcial1-YersonEscolastico/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Parcial1_YersonEscolastico.Entidades;
+using System.Data.Entity;
+
+
+namespace Parcial1_YersonEscolastico.DAL
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Productos> Producto { get; set; }
+
+        public Contexto() : base("ConStr")
+        { }
+    }
+}
--- Parcial1-YersonEscolastico/Entidades/Productos.cs
+++ Parcial1-YersonEscolastico/Entidades/Productos.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Parcial1_YersonEscolastico.Entidades
 {
-    class Productos
+    public class Productos
     {
 
         public int ProductoId { get; set; }

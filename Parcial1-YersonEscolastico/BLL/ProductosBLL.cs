e721b2993b55993b1bf0ba009695dd69095fbac6
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-05-31 10:08:31 +0800

    Agregando clase ProductosBLL

---
+++ Parcial1-YersonEscolastico/BLL/ProductosBLL.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Parcial1_YersonEscolastico.Entidades;
+using Parcial1_YersonEscolastico.DAL;
+using System.Linq.Expressions;
+
+namespace Parcial1_YersonEscolastico.BLL
+{
+    class ProductosBLL
+    {
+
+        public static bool Guardar(Productos productos)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                if (contexto.Producto.Add(productos) != null)
+                    paso = contexto.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Modificar(Productos productos)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                db.Entry(productos).State = System.Data.Entity.EntityState.Modified;
+                paso = (db.SaveChanges() > 0);
+                db.Dispose();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                var eliminar = contexto.Producto.Find(id);
+                contexto.Entry(eliminar).State = System.Data.Entity.EntityState.Deleted;
+                paso = (contexto.SaveChanges() > 0);
+
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+
+
+        public static Productos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Productos productos = new Productos();
+            try
+            {
+                productos = contexto.Producto.Find(id);
+            }
+            catch
+            {
+                throw;
+
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return productos;
+        }
+
+
+        public static List<Productos> GetList(Expression<Func<Productos, bool>> productos)
+        {
+            Contexto contexto = new Contexto();
+            List<Productos> lista = new List<Productos>();
+            try
+            {
+                lista = contexto.Producto.Where(productos).ToList();
+
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+
+    }
+}

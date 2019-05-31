23d69f0447b12ec79d08290fdcc6fd65dc9801ad
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-05-31 09:47:25 +0800

    Agregando entidad Productos

---
+++ Parcial1-YersonEscolastico/DAL/Scripts/Parcial1Db.sql
@@ -0,0 +1,19 @@
+Create database Parcial1Db;
+
+go
+
+use Parcial1Db
+
+Create table Productos(
+ 
+ productoId int primary key identity,
+ descripcion varchar(30),
+ Existencia float,
+ costo float,
+ valorInventario float
+)
+
+
+
+
+
---
+++ Parcial1-YersonEscolastico/Entidades/Productos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_YersonEscolastico.Entidades
+{
+    class Productos
+    {
+
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set; }
+        public float Existencia { get; set; }
+        public float Costo { get; set; }
+        public float ValorInventario { get; set; }
+
+        public Productos()
+        {
+            ProductoId = 0;
+            Descripcion = string.Empty;
+            Existencia = 0.0f;
+            Costo = 0.0f;
+            ValorInventario = 0.0f;
+        }
+    }
+}

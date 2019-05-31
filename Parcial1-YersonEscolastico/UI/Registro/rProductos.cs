905ede5db741c8cde1d16cf7cdbe9572c4f7417a
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-06-01 00:52:25 +0800

    Arreglando un error en las validaciones

--- Parcial1-YersonEscolastico/UI/Registro/rProductos.cs
+++ Parcial1-YersonEscolastico/UI/Registro/rProductos.cs
@@ -111,7 +111,7 @@ namespace Parcial1_YersonEscolastico.UI.Registro
             Productos productos;
             bool paso = false;
 
-            if (Validar())
+            if (!Validar())
                 return;
 
             productos = LlenaClase();

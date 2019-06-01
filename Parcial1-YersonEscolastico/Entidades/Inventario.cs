fb3892b36337fbe192001aa264481d91950766a6
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-06-01 23:20:56 +0800

    Agregando entidad inventario

---
+++ Parcial1-YersonEscolastico/Entidades/Inventario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_YersonEscolastico.Entidades
+{
+    public class Inventario
+    {
+        [Key]
+        public int Id { get; set; }
+        public float Total { get; set; }
+
+        public Inventario()
+        {
+            Id = 0;
+            Total = 0;
+        }
+    }
+}

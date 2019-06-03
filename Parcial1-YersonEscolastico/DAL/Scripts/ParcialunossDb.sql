a8749c33409f5e005c70b56083d5d30f168d36a7
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-06-04 04:54:18 +0800

    Haciendo otro cambio para finalizar

--- Parcial1-YersonEscolastico/DAL/Scripts/ParcialunossDb.sql
+++ Parcial1-YersonEscolastico/DAL/Scripts/ParcialunossDb.sql
@@ -24,8 +24,8 @@ delete from Inventarios where id=1
 select * from Productos
 select * from Inventarios
 
---SET IDENTITY_INSERT Table1 ON
---insert into Inventarios (Id, Total) values ('1','0');
+SET IDENTITY_INSERT Inventarios ON
+insert into Inventarios (Id, Total) values ('1','0');
 
 
 

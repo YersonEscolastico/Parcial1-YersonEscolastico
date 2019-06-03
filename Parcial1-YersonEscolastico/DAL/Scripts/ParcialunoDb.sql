3acb0acf79281422e1e30918ca4b6e152cf20324
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-06-03 22:34:49 +0800

    Terminando

---
+++ Parcial1-YersonEscolastico/DAL/Scripts/ParcialunoDb.sql
@@ -0,0 +1,33 @@
+--Create database ParcialunoDb;
+
+--go
+
+--use Parcial1Db
+
+--Create table Productos(
+ 
+-- productoId int primary key identity,
+-- descripcion varchar(30),
+-- Existencia float,
+-- costo float,
+-- valorInventario float
+--)
+
+
+--Create table Inventario(
+ 
+-- Id int primary key identity,
+-- float Total
+--)
+
+
+select * from Productos
+select * from Inventarios
+
+--insert into Inventarios (Id, Total) values ('','0');
+
+
+
+
+
+
